7fb59f33de75de411b42c287937a3ad66c1cb744
Joanna Delaporte <joannadelaporte@gmail.com>
2017-03-02 12:54:03 -0600

    Script now checks a list to NOT delete some users.

--- delete_users.sh
+++ delete_users.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 #JRD
-#SCript to delete users and home directories
+#Script to delete users and home directories
 #Let's make sure they are student users, first.
+#Let's record name, UID, and result to a file
 #userdel 
 # -r removes home and mail spool
- 
+
 names=( $(cat csit_inactive_unix.csv) )
-outfile=users_login_record.txt
+outfile=delete_users_output.txt
 year1=2016
-year2=2017
+year2=2015
 tempuid=""
+keepusers=users_to_NOT_delete.txt
 
 for name in "${names[@]:0:10}"
 do
-	tempuid=$(getent passwd ${name} | cut -d : -f3)
+tempuid=$(getent passwd ${name} | cut -d : -f3)
+
+if grep -q ${name} ${keepusers}
+then
+		echo "" >> $outfile
+		echo "user: $name" >> $outfile
+		echo "    UID: $tempuid" >> $outfile
+		echo "    result: ${name} is a staff user. Do NOT delete." | tee >> $outfile
+
+else
 	if [[ -z $tempuid ]]
 	then tempuid="No uid found"
  
-	elif [[ $tempuid -gt 1001 && $tempuid -le 1020 ]]
+	elif [[ $tempuid -gt 1001 && $tempuid -le 2020 ]]
 	then
 		echo "" >> $outfile
 		echo "user: $name" >> $outfile
@@ -25,15 +36,15 @@ do
 		last ${name} | cut -d ' ' -f8 | egrep "${year1}|${year2}"
 			if [[ $? -eq 0 ]]
 			then
-				echo "    message: Leaving user intact for now since login occurred in $year1 or $year2." | tee >> $outfile
+				echo "    result: Leaving user intact for now since login occurred in $year1 or $year2." | tee >> $outfile
 			else
-				echo "    message: Removing ${name} user" | tee >> $outfile 
-				userdel -r ${name} | tee >> $outfile
+				echo "    result: Removing ${name} user" | tee >> $outfile 
+#				userdel -r ${name} | tee >> $outfile
 			fi
 	else
 		echo "" >> $outfile
 		echo "user: $name" >> $outfile
-		echo "    message: ${name} is not in the desired user range" | tee >> $outfile
+		echo "    result: ${name} is not in the desired user range" | tee >> $outfile
 	fi
+fi
 done
-
