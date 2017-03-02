a929205e3880322724c3aefea8d49a28b7d1029d
Joanna Delaporte <joannadelaporte@gmail.com>
2017-03-02 12:04:08 -0600

    Very working add and delete users scripts.
    The delete script checks UID and compares to two login years before deleting users.

--- delete_users.sh
+++ delete_users.sh
@@ -6,16 +6,34 @@
 # -r removes home and mail spool
  
 names=( $(cat csit_inactive_unix.csv) )
+outfile=users_login_record.txt
+year1=2016
+year2=2017
+tempuid=""
 
 for name in "${names[@]:0:10}"
 do
-	tempuid=$(getent passwd ${name} | cut -d : -f3) 
-	if [[ $tempuid -gt 1001 && $tempuid -le 1011 ]]
+	tempuid=$(getent passwd ${name} | cut -d : -f3)
+	if [[ -z $tempuid ]]
+	then tempuid="No uid found"
+ 
+	elif [[ $tempuid -gt 1001 && $tempuid -le 1020 ]]
 	then
-		echo "Removing ${name} user" 
-		userdel -r ${name}
+		echo "" >> $outfile
+		echo "user: $name" >> $outfile
+		echo "    UID: $tempuid" >> $outfile
+		last ${name} | cut -d ' ' -f8 | egrep "${year1}|${year2}"
+			if [[ $? -eq 0 ]]
+			then
+				echo "    message: Leaving user intact for now since login occurred in $year1 or $year2." | tee >> $outfile
+			else
+				echo "    message: Removing ${name} user" | tee >> $outfile 
+				userdel -r ${name} | tee >> $outfile
+			fi
 	else
-		echo "${name} is not in the desired user range"
+		echo "" >> $outfile
+		echo "user: $name" >> $outfile
+		echo "    message: ${name} is not in the desired user range" | tee >> $outfile
 	fi
 done
 
