78feb76e4212ac09dd7dce694c3db6c3cfd48800
Joanna Delaporte <joannadelaporte@gmail.com>
2017-03-02 16:14:53 -0600

    Updated script

--- delete_users.sh
+++ delete_users.sh
@@ -5,18 +5,18 @@
 #Let's record name, UID, and result to a file
 #userdel 
 # -r removes home and mail spool
-
 names=( $(cat csit_inactive_unix.csv) )
 outfile=delete_users_output.txt
 year1=2016
 year2=2015
 tempuid=""
 keepusers=users_to_NOT_delete.txt
+userhome=""
 
 for name in "${names[@]:0:10}"
 do
 tempuid=$(getent passwd ${name} | cut -d : -f3)
-
+userhome=$(getent passwd ${name} | cut -d : -f6)
 if grep -q ${name} ${keepusers}
 then
 		echo "" >> $outfile
@@ -33,11 +33,13 @@ else
 		echo "" >> $outfile
 		echo "user: $name" >> $outfile
 		echo "    UID: $tempuid" >> $outfile
-		last ${name} | cut -d ' ' -f8 | egrep "${year1}|${year2}"
+		last ${name} | egrep "${year1}|${year2}"
 			if [[ $? -eq 0 ]]
 			then
 				echo "    result: Leaving user intact for now since login occurred in $year1 or $year2." | tee >> $outfile
 			else
+				echo 'echo "Your account is marked for deletion from shaula. Please contact the Tech Help Desk immediately if you believe this is an error."' >> ${userhome}/.bash_profile
+				echo 'echo "Your account is marked for deletion from shaula. Please contact the Tech Help Desk immediately if you believe this is an error."' >> ${userhome}/.profile
 				echo "    result: Removing ${name} user" | tee >> $outfile 
 #				userdel -r ${name} | tee >> $outfile
 			fi
